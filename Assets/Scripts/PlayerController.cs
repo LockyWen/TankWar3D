0f554d0ed5febd0ee3774140eb5bff7662ac772d
Mincong Wen <61830237+LockyWen@users.noreply.github.com>
2020-10-23 00:47:00 -0400

    project set up

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float horizontal;
+    float vertical;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        transform.Rotate(new Vector3(0, horizontal, 0));
+        transform.Translate(new Vector3(0, 0, vertical*0.05f), Space.Self);
+    }
+}
---
+++ Assets/Scripts/Tools.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Tools
+{
+    public static Transform FindChildByName(this Transform parent, string name)
+    {
+        if(parent.name == name)
+        {
+            return parent;
+        }
+
+        if(parent.childCount == 0)
+        {
+            return null;
+        }
+
+        Transform target = null;
+        target.Find(name);
+        if(target != null)
+        {
+            return target;
+        }
+
+        for(int i = 0; i < parent.childCount; i++)
+        {
+            target = FindChildByName(parent.GetChild(i), name);
+        }
+
+        return target;
+    }
+}
